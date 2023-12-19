715a6fd1a49b5362221dea8d04ed95e51aaa9324
hongcha89 <jericho8901@gmail.com>
2023-12-19 11:22:58 +0900

    the trio is modified

--- src/Trio.java
+++ src/Trio.java
@@ -1,25 +1,44 @@
 
 import java.util.*;
-
 public class Trio {
 
     public int solution(int[] number) {
-        number = new int[]{-3, -2, -1, 0, 1, 2, 3};
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int i = 0; i < number.length; i++) {
-            for (int j = 0; j < i; j++) {
-                for (int k = 0; k < j; k++) {
-                    if (number[i] + number[j] + number[k] == 0) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(number[i]);
-                        list.add(number[j]);
-                        list.add(number[k]);
-                        lists.add(list);
+        int first = 0;
+        int second = 0;
+        Set<List<Integer>> set = new HashSet<>();
 
-                    }
+        for (int  i = 0; i < number.length; i++) {
+            List<Integer> list = new ArrayList<>();
+            second = makeEachPlus(second);
+            int towSum = number[second] + number[first];
+            list.add(towSum);
+
+            List<Integer> listWhereTrio = new ArrayList<>();
+            for(int j=first; j<=second; j++) {
+                if (((list.get(0) + number[j]) == 0)  && second!=j&& j!=first && first!=second) {
+
+                    listWhereTrio.add(first);
+                    listWhereTrio.add(second);
+                    listWhereTrio.add(j);
+
+                    Collections.sort(listWhereTrio);
+                    set.add(listWhereTrio);
                 }
             }
+            if (second == number.length - 1) {
+                second = i=0;
+                first++;
+            }
+
+            if (first == number.length - 1) {
+                break;
+            }
         }
-        return lists.size();
+
+        return set.size();
+    }
+
+    private int makeEachPlus(int each) {
+        return ++each;
     }
 }
