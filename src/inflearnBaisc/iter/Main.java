3e25ca57300645ae9dd8a8e12e841d9a561b2ad2
seohongik <jericho8901@gmail.com>
2025-05-25 23:29:07 +0900

    iter and di(aop)

--- src/inflearnBaisc/iter/Main.java
+++ src/inflearnBaisc/iter/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
         System.out.println("==============makeData Start==================");
 
-        List<Integer> arrayList = List.of(1,2,3);
-        //arrayList.add(1);
+        List<Integer> arrayList = List.of(1,2,3); // 불변 of 모두 불변
+        //arrayList.add(1); // 가변
         //arrayList.add(2);
         //arrayList.add(3);
 
-        List<Integer> linkedList = new LinkedList<>(arrayList);
+        List<Integer> linkedList = new LinkedList<>(arrayList); // 가변
         Set<Integer> hashSet =Set.of(1,3,2,4);
 
         //hashSet.add(1);
@@ -30,6 +30,8 @@ public class Main {
         //hashMap.put(3, 3);
         Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>(hashMap);
         Map<Integer, Integer> treeMap = new TreeMap<>(linkedHashMap);
+
+
 
         System.out.println("==============forEach Start==================");
         foreach(arrayList);
@@ -59,7 +61,14 @@ public class Main {
     public static void foreach(Collection<?> collection) {
         System.out.println(collection.getClass().getName());
         for (Object o : collection) {
-            System.out.println(o);
+            if(o instanceof Map.Entry) {
+                Object value=((Map.Entry<?, ?>) o).getValue();
+                Object key =  ((Map.Entry<?, ?>) o).getKey();
+                System.out.println("key :"+key +"  value :"+value);
+            }else {
+                System.out.println("element : "+o);
+            }
+
         }
     }
 
