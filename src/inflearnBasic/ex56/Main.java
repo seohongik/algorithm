f35fdf87133785832622ebcf15641fff252e1dca
seohongik <jericho8901@gmail.com>
2025-06-28 20:05:57 +0900

    팩토리얼 피보나치 메모라이제이션 까지

--- src/inflearnBasic/ex56/Main.java
+++ src/inflearnBasic/ex56/Main.java
@@ -2,31 +2,36 @@ package inflearnBasic.ex56;
 
 import java.util.Scanner;
 
-class Solution {
-    public int   solution(int n) {
+public class Main {
+
+    static int [] memo;
+    public static int solution(int n) {
+
+        if(memo[n] > 0) return memo[n]; // 이미 값이 있을때 구해져 있는 값을 이용한다.
+
         if(n == 1) {
-            return 1;
+            return memo[n]=1;
         } else if(n == 2) {
-            return 1;
+            return memo[n]=1;
         }else {
-            return solution(n - 1) + solution(n - 2);
+            return memo[n] = solution(n - 2)+solution(n - 1);
+
         }
 
     }
-    
-
-
-}
-public class Main {
-
     public static void main(String[] args){
-        Solution solution = new Solution();
         Scanner kb = new Scanner(System.in);
 
         int n = kb.nextInt();
 
+        //for(int i = 1; i <= n; i++) {
+        //    System.out.print(solution.solution(i) + " ");
+        //}
+
+        memo = new int[n+1];
+        solution(n);
         for(int i = 1; i <= n; i++) {
-            System.out.print(solution.solution(i) + " ");
+            System.out.println(memo[i]+" ");
         }
     }
 }
