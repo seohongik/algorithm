12a7953fe90738d0e60541aee74c6ee6ec09f73d
seohongik <jericho8901@gmail.com>
2025-06-29 19:06:07 +0900

    빈 스페이스 삭제

--- src/inflearnBasic/ex56/Main.java
+++ src/inflearnBasic/ex56/Main.java
@@ -21,13 +21,11 @@ public class Main {
     }
     public static void main(String[] args){
         Scanner kb = new Scanner(System.in);
-
         int n = kb.nextInt();
 
         //for(int i = 1; i <= n; i++) {
         //    System.out.print(solution.solution(i) + " ");
         //}
-
         memo = new int[n+1];
         solution(n);
         for(int i = 1; i <= n; i++) {
