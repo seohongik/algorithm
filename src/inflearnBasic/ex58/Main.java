012671a016d9e9b081d0b9b8ae1e8977c03686fc
seohongik <jericho8901@gmail.com>
2025-06-30 06:03:27 +0900

    부분집함 ( 결론 재귀는 한번만 돌리자)

--- src/inflearnBasic/ex58/Main.java
+++ src/inflearnBasic/ex58/Main.java
@@ -5,38 +5,38 @@ import java.util.Arrays;
 
 class Solution {
 
+    static int count = 0;
     int n;
     boolean[] printable;
     public void  solution(int tree) {
 
-        if(tree == n+1) {
 
-           for (int i = 0; i < printable.length; i++) {
+        if(tree == n+1) {
+            System.out.println("출력으로  " + tree);
+            for (int i = 0; i < printable.length; i++) {
                if(printable[i]) {
                    System.out.print((i+1)+ " ");
                }
-           }
-           System.out.println();
+            }
+            System.out.println();
 
         }else {
+
+            System.out.println( "1스텍 위  "+tree+"  "+  printable[tree-1] +" " +"안 찍히는 값 :: "+tree);
             printable[tree-1] = true;
-
-            System.out.println( "첫번째 사이클 "+tree+"  "+  printable[tree-1] +" " +"찍히는 값 :: "+tree);
-
+            System.out.println( "상태 값 변경 "+  printable[tree-1] +" 변경 인덱스  "+ tree);
             solution(tree+1);
-
-            System.out.println( "두번째 사이클 "+tree+"  "+  printable[tree-1] +" " +"찍 찍히는 값 :: "+tree);
-
             printable[tree-1] = false;
-
-            System.out.println( "셋번째 사이클 "+tree+"  "+  printable[tree-1] +" " +"안 찍히는 값 :: "+tree);
-
+            System.out.println( "1스텍 아래 2스텍 위  "+tree+"  "+  printable[tree-1] +" " +"찍히는 값 :: "+tree);
+            System.out.println( "상태  값 변경 "+  printable[tree-1] +" 변경 인덱스  "+ tree);
             solution(tree+1);
-
-            System.out.println( "넷번째 사이클 "+tree+"  "+  printable[tree-1] +" " +"안 찍히는 값 :: "+tree);
+            System.out.println( "2스텍 아래  "+tree+"  "+  printable[tree-1]);
 
         }
+        count++;
+        System.out.println("count = " + count);
 
+        System.out.println("출발점은" );
         return;
     }
 
