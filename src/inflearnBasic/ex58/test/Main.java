791a656cd6005f97b25aa01aad83de04c7143d61
seohongik <jericho8901@gmail.com>
2025-06-30 14:52:40 +0900

    재귀 스텍프레임 동작 원리

--- src/inflearnBasic/ex58/test/Main.java
+++ src/inflearnBasic/ex58/test/Main.java
@@ -3,35 +3,23 @@ package inflearnBasic.ex58.test;
 
 class Solution {
 
-    static int count = 0;
-    int n;
-    boolean[] printable;
     public void  solution(int tree) {
 
 
         if(tree<=0) {
-            System.out.println("출력으로  " + tree);
-            for (int i = 0; i < printable.length; i++) {
-               if(printable[i]) {
-                   System.out.print((i+1)+ " ");
-               }
-            }
-            System.out.println();
+
+            System.out.println("4    tree " + tree );
 
         }else {
 
-            System.out.println( "1스텍 위");
-            printable[tree-1] = true;
+            System.out.println("1    tree " + tree );
             solution(tree-1);
-            printable[tree-1] = false;
-            System.out.println( "1스텍 아래 2스텍 위");
+            System.out.println("2    tree " + tree );
             solution(tree-1);
-            System.out.println( "2스텍 아래");
+            System.out.println("3    tree " + tree );
+            System.out.println();
 
         }
-        count++;
-        System.out.println("count = " + count);
-        System.out.println("출발점은" );
         return;
     }
 
@@ -41,9 +29,7 @@ class Main{
 
     public static void main(String[] args){
         Solution sol = new Solution();
-        sol.n = 3;
-        sol.printable = new boolean[sol.n];
-        sol.solution(sol.n);
+        sol.solution(3);
 
 
     }
