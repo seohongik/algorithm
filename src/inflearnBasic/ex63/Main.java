56fe19ce6b45d4325ef7af81ceb17fac12008d54
seohongik <jericho8901@gmail.com>
2025-07-26 14:40:03 +0900

    Graph DFS 알다가도 모르겠다..

---
+++ src/inflearnBasic/ex63/Main.java
@@ -0,0 +1,91 @@
+package inflearnBasic.ex63;
+
+import java.util.*;
+
+/*
+
+5 9
+1 2
+1 3
+1 4
+2 1
+2 3
+2 5
+3 4
+4 2
+4 5
+
+
+* */
+
+//GRAPH
+class Solution {
+
+    int n=0;
+    int m=0;
+    int answer = 0;
+    boolean[] visited;
+    int[][] graph;
+
+    public int solution(int start) {
+
+
+        /*  0  1  2  3  4  5
+        0  [0, 0, 0, 0, 0, 0],
+        1  [0, 0, 1, 1, 1, 0],
+        2  [0, 1, 0, 1, 0, 1],
+        3  [0, 0, 0, 0, 1, 0],
+        4  [0, 0, 1, 0, 0, 1],
+        5  [0, 0, 0, 0, 0, 0]
+         */
+
+        /*
+        1 -> 2 -> 3 -> 4 -> 5
+        1 -> 2 -> 5
+        1 -> 3 -> 4 -> 2 -> 5
+        1 -> 3 -> 4 -> 5
+        1 -> 4- > 2 -> 5
+        1 -> 4 -> 5
+        */
+
+        if(start==n) answer++;
+        else{
+            for(int i=1; i<=n; i++){
+                if(graph[start][i]==1 && !visited[i]){
+                    visited[i]=true;
+                    solution(i);
+                    visited[i]=false;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+}
+
+
+class Main{
+
+
+
+    public static void main(String[] args){ // 예상결과 2
+
+        Solution sol = new Solution();
+        Scanner kb = new Scanner(System.in);
+        sol.n=kb.nextInt();
+        sol.m=kb.nextInt();
+        sol.graph=new int[sol.n+1][sol.n+1];
+        sol.visited=new boolean[sol.n+1];
+        for(int i=0; i<sol.m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            sol.graph[a][b]=1;
+        }
+        sol.visited[1]=true;
+        sol.solution( 1);
+        System.out.println(sol.answer);
+        
+    }
+}
