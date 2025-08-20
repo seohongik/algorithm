2ba76b0582040097ea4ff73a957d4e5c76287aa3
seohongik <jericho8901@gmail.com>
2025-08-20 11:45:47 +0900

    프로그래머스 n 부터 시작하는 원소

--- src/programus/FromN.java
+++ src/programus/FromN.java
@@ -6,49 +6,21 @@ import java.util.List;
 
 public class FromN {
     public int[] solution(int[] num_list, int n) {
+
+        //List<Integer> list = new ArrayList<>();
         int count = 0;
-        int index = 0;
-
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] == n) {
-                index = i;
-                break;
-            }
-            count++;
+        int[] answer = new int[num_list.length-n+1];
+        for (int i = n-1; i < num_list.length; i++) {
+            //list.add(num_list[i]);
+            answer[count++]=num_list[i];
         }
 
-        int[] answer = new int[num_list.length - count];
-
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = num_list[index++];
-        }
-
+        //return list.stream().mapToInt(i -> i).toArray();
         return answer;
-    }
-
-    public int[] solution2(int[] num_list, int n) {
-        int index = 0;
-
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] == n) {
-                index = i;
-                break;
-            }
-        }
-
-
-        for (int i = index; i < num_list.length; i++) {
-            list.add(num_list[i]);
-        }
-
-        return list.stream().mapToInt(i -> i).toArray();
     }
 
     public static void main(String[] args) {
         FromN solution = new FromN();
-        System.out.println(Arrays.toString(solution.solution(new int[]{2, 1, 6}, 6)));
+        System.out.println(Arrays.toString(solution.solution(new int[]{5,2,1,7,5}, 2)));
     }
 }
