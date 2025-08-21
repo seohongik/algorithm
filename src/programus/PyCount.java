642620eafeb92654e3d8e1f07b1ea69f939d90f1
seohongik <jericho8901@gmail.com>
2025-08-22 01:52:14 +0900

    프로그래머스 P와Y의 개수

---
+++ src/programus/PyCount.java
@@ -0,0 +1,35 @@
+package programus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PyCount {
+
+    boolean solution(String s) {
+        s = s.toLowerCase();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        int pCount = map.getOrDefault('p',-1);
+        int yCount = map.getOrDefault('y',-1);
+
+        if(pCount == -1 || yCount == -1){
+            return false;
+        }
+
+        if(pCount==yCount){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        PyCount p = new PyCount();
+        System.out.println(p.solution("pPoooyY"));
+
+        System.out.println(p.solution("Pyy"));
+        System.out.println(p.solution("o"));
+    }
+}
