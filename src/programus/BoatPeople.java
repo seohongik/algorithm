ff20bf64dfd8a7a61ca46008853418b49b65b71d
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-09-29 14:59:26 +0900

    Update BoatPeople.java

--- src/programus/BoatPeople.java
+++ src/programus/BoatPeople.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 public class BoatPeople {
 
     public int solution(int[] people, int limit) {
+        //구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 합니다.
         Arrays.sort(people); // 정렬
         int ans = 0;
         int start=0; // 투포인터 시작점 증가 변수
@@ -13,7 +14,7 @@ public class BoatPeople {
                 start++; // 테웠으면 가벼운 사람 증가
             }
             end--; // 무게있는 사람은 무조건 감소
-            ans++;
+            ans++; // 보트 타기
         }
         return ans;
     }
@@ -21,8 +22,8 @@ public class BoatPeople {
     public static void main(String[] args) {
         //https://school.programmers.co.kr/learn/courses/30/lessons/42885
         BoatPeople boatPeople = new BoatPeople();
-        boatPeople.solution(new int[]{1,2,3,4,5,6,7,8,9,10},10);
-        boatPeople.solution(new int[]{70, 50, 80, 50},100); //3
-        boatPeople.solution(new int[]{70, 80, 50},100); //3
+        System.out.println(boatPeople.solution(new int[]{1,2,3,4,5,6,7,8,9,10},10)); // [10] [1,9] [2,8] [3,7] [4,6] [5]
+        System.out.println(boatPeople.solution(new int[]{70, 50, 80, 50},100)); //3 -> 50 50 70 80 -> [50 50] [70] [80]
+        System.out.println(boatPeople.solution(new int[]{70, 80, 50},100)); //3 -> 50 70 80 [50] [70] [80]
     }
 }
