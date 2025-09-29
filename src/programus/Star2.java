8a785cfc6f495eb0b39b959f8a097948049d031b
seo-hongik <110377614+seohongik@users.noreply.github.com>
2025-09-29 15:52:30 +0900

    Update Star2.java
    별찍기

--- src/programus/Star2.java
+++ src/programus/Star2.java
@@ -1,8 +1,8 @@
+package progamus;
 public class Star2 {
 
     public static void main(String[] args) {
         for (int i = 0; i <5 ; i++) {
-
             for (int j = i; j < 5; j++) {
                 System.out.print("*");
             }
@@ -10,7 +10,6 @@ public class Star2 {
         }
         System.out.println("=============================");
         for (int i = 0; i <5 ; i++) {
-
             for (int j = 0; j < i+1; j++) {
                 System.out.print("*");
             }
@@ -35,9 +34,7 @@ public class Star2 {
             }
             System.out.println();
         }
-
         System.out.println("=============================");
-
         /*
 
         데이터 아래와 같이 만들기
@@ -45,7 +42,6 @@ public class Star2 {
            * * *
              *
         */
-
         for (int i=1; i <=3 ; i++) {
             for (int j=1; j<=9; j++){
                 if(i==1&& (j==4 || j==6)){
@@ -62,6 +58,40 @@ public class Star2 {
                     System.out.print(" ");
                 }
             }
+            System.out.println();
+        }
+
+        /*
+        데이터 아래와 같이 만들기
+             *
+            ***
+           *****
+          *******
+         *********
+         000010000
+         000111000
+         001111100
+         011111110
+         111111111
+       */
+        System.out.println("=============================");
+        int back = 10/2;
+        int prev = 10/2;
+        for (int i = 1; i <=5 ; i++) {
+            for (int j =1; j <= 9; j++) {
+                /* 1 2 3 4  6 7 8 9
+                   1 2 3      7 8 9
+                   1 2          8 9
+                   1              9
+                */
+                if(back<=j&&j<=prev) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            back--;
+            prev++;
             System.out.println();
         }
     }
