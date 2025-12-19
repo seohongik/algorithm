cf2894448f5ba15a87b63e3c1e8fef78bfb9de3a
seohongik <jericho8901@gmail.com>
2025-12-19 12:45:00 +0900

    프로그래머스 다트게임

--- src/programus/DartGame.java
+++ src/programus/DartGame.java
@@ -5,72 +5,65 @@ import java.util.List;
 import java.util.Stack;
 
 public class DartGame {
+
     public int solution(String dartResult) {
         List<Integer> list = new ArrayList<>();
         int score = 0;
         for (int i = 0; i < dartResult.length(); i++) {
-            if(Character.isDigit(dartResult.charAt(i) )) {
-                score = (int)(dartResult.charAt(i))-48;
+            if (Character.isDigit(dartResult.charAt(i))) {
+                score = (int) (dartResult.charAt(i)) - 48;
 
             }
-            if(i>=1) {
+            if (i >= 1) {
                 if (dartResult.charAt(i - 1) == '1' && dartResult.charAt(i) == '0') {
                     score = 10;
                 }
             }
 
-            if(dartResult.charAt(i) == '*') {
+            if (dartResult.charAt(i) == '*') {
                 list.add(Integer.MAX_VALUE);
             }
-            if(dartResult.charAt(i) == '#') {
+            if (dartResult.charAt(i) == '#') {
                 list.add(Integer.MIN_VALUE);
             }
 
             if (dartResult.charAt(i) == 'S') {
-                list.add((int)Math.pow(score, 1));
-            }else if (dartResult.charAt(i) == 'D') {
-                list.add((int)Math.pow(score, 2));
-            }else if (dartResult.charAt(i) == 'T') {
-                list.add((int)Math.pow(score, 3));
+                list.add((int) Math.pow(score, 1));
+            } else if (dartResult.charAt(i) == 'D') {
+                list.add((int) Math.pow(score, 2));
+            } else if (dartResult.charAt(i) == 'T') {
+                list.add((int) Math.pow(score, 3));
             }
         }
-        int index =0;
-        if(list.get(1)==Integer.MAX_VALUE) {
-            list.set(0,list.get(0)*2);
+        int index = 0;
+        if (list.get(1) == Integer.MAX_VALUE) {
+            list.set(0, list.get(0) * 2);
             list.remove(1);
-            index=2;
+            index = 2;
         }
 
-        while (index<list.size()-1){
+        while (index < list.size()) {
 
 
-            if(list.get(index)==Integer.MAX_VALUE) {
-                list.set(index-2,list.get(index-2)*2);
-                list.set(index-1,list.get(index-1)*2);
+            if (list.get(index) == Integer.MAX_VALUE) {
+                list.set(index - 2, list.get(index - 2) * 2);
+                list.set(index - 1, list.get(index - 1) * 2);
                 list.remove(index);
-                index=0;
+                index = 0;
             }
 
-            if(list.get(index)==Integer.MIN_VALUE) {
-                list.set(index-1,list.get(index-1)*-1);
+            if (list.get(index) == Integer.MIN_VALUE) {
+                list.set(index - 1, list.get(index - 1) * -1);
                 list.remove(index);
-                index=0;
+                index = 0;
             }
 
             index++;
         }
 
-        if(list.get(list.size()-1)==Integer.MAX_VALUE) {
-            list.set(list.size()-3,list.get(list.size()-3)*2);
-            list.set(list.size()-2,list.get(list.size()-2)*2);
-            list.remove(list.size()-1);
-        }
-        if(list.get(list.size()-1)==Integer.MIN_VALUE) {
-            list.set(list.size()-2,list.get(list.size()-2)*-1);
-            list.remove(list.size()-1);
-        }
-        return list.stream().mapToInt(i->i).sum();
+        return list.stream().mapToInt(i -> i).sum();
     }
+
 
     public static void main(String[] args) {
         DartGame game = new DartGame();
