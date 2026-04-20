8e2f1fe95301d025a412469213fcbcbbf1ff51bb
seohongik <jericho8901@gmail.com>
2026-04-21 02:37:08 +0900

    프로그래머스 스포일러 방지 https://school.programmers.co.kr/learn/courses/30/lessons/468370?language=java

---
+++ src/programus/SpoilerWords.java
@@ -0,0 +1,55 @@
+package programus;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SpoilerWords {
+    public int solution(String message, int[][] spoiler_ranges) {
+        int answer = 0;
+
+        String[] mask = new String[message.length()];
+
+        for (int i = 0; i < mask.length; i++) {
+            mask[i] = message.substring(i, i + 1);
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+
+             for (int j = 0; j < spoiler_ranges.length; j++) {
+                 boolean add=spoiler_ranges[j][0]<=i&&i<spoiler_ranges[j][1]+1;
+                 if(add) {
+                    mask[i] = "*";
+                 }
+                 if(" ".equals(String.valueOf(message.charAt(i)))){
+                     mask[i] = " ";
+                 }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(mask).forEach(m->{
+            sb.append(m);
+        });
+        String[] masks = sb.toString().split(" ");
+        String[] origin = message.split(" ");
+        Map<String, Integer > map = new LinkedHashMap<>();
+
+        for (int i = 0; i < origin.length; i++) {
+
+            map.put(origin[i], map.getOrDefault(origin[i], 0) + 1);
+            if (masks[i].contains("*")) {
+                map.put(origin[i], map.getOrDefault(origin[i], 0) - 1);
+            }
+        }
+        answer=(int)map.values().stream().filter(i->i<=0).count();
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SpoilerWords spoilerWords = new SpoilerWords();
+        spoilerWords.solution("here is muzi here is a secret message",new int[][]{{0, 3}, {23, 28}});
+        spoilerWords.solution("my phone number is 01012345678 and may i have your phone number",  new int[][]{{5, 5}, {25, 28}, {34, 40}, {53, 59}});
+    }
+}
